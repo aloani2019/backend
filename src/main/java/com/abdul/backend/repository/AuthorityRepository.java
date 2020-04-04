160047bcbdd7ac8b81c85914e9dfa4341896e199
aloani20 <bumi3times@gmail.com>
2020-04-04 14:20:51 +0300

    init

---
+++ src/main/java/com/abdul/backend/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.abdul.backend.repository;
+
+import com.abdul.backend.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
